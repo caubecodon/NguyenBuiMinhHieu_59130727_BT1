dad9998e68d0da9bdfdab4afeb3fb520159d3217
Hieu <nguyenhiu99@gmail.com>
2020-04-13 21:42:27 +0700

    toang

--- src/AbstractClass/GiaoVien.java
+++ src/AbstractClass/GiaoVien.java
@@ -41,5 +41,10 @@ public class GiaoVien extends CaNhan {
       thongtin = "ho ten: " + this.hoten + ", tuoi: " + this.tuoi + ", dia chi: " + this.diachi
         + ", sdt: " + this.sdt + ", mon day: " + this.mon + ", to bo mon: " + this.bomon;
       return thongtin;
+    // alert : The assigned value is never used
+    // Điều đó có nghĩa là bạn không làm gì với biến đó sau khi bạn đã gán giá trị cho biến đó.
+    //Ví dụ, bạn không trả lại nó, thực hiện một phép tính với nó, chuyển nó cho một phương thức hoặc bất cứ thứ gì khác.
+    //Về cơ bản, điều đó có nghĩa là biến không được sử dụng sau khi giá trị được gán cho nó.
+    //biến chỉ sử dụng để xuất thông tin giáo viên và học sinh nhưng không được gọi lại hay tính toán gì
     }
 }
